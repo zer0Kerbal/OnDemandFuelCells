06951feb8fc4cf8fc790ff4b5bb68edcae5a8ed9
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2019-07-03 01:05:11 -0500

    initial update to VStudio
    update/move AssemblyInfo.cs
    create OnDemandFuelCells.csproj
    update OnDemandFuelCells.version
    create OnDemandFuelCells.sln

---
+++ OnDemandFuelCells/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ODFC")]
+[assembly: AssemblyDescription("On Demand Fuel Cells")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Okram Industries")]
+[assembly: AssemblyProduct("ODFC")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fefd366-35f6-4219-a448-7de4054204f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.7.2.0")]
+[assembly: AssemblyFileVersion("1.7.2.0")]
