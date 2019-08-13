120a5d4c2ff63f19663863c8f7819d825bf83418
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2019-08-13 13:23:15 -0500

    4x4cheesecake

--- OnDemandFuelCells/cfg.cs
+++ OnDemandFuelCells/cfg.cs
@@ -4,134 +4,134 @@ using System;
 using UnityEngine;
 
 namespace ODFC {
-	public class rla {
-		public int rid;
-		public string shrt;
+	public class rla { //resource la? 
+		public int resourceID;
+		public string ressourceAbbreviation;
 
-		public rla(int rid, string shrt) {
-			this.rid = rid;
-			this.shrt = shrt;
+		public rla(int resourceID, string ressourceAbbreviation) {
+			this.resourceID = resourceID;
+			this.ressourceAbbreviation = ressourceAbbreviation;
 		}
 	}
 
-	public struct fuel {
-		public int rid;
+	public struct Fuel {
+		public int resourceID;
 		public float rate;
-		public ResourceFlowMode rfm;
+		public ResourceFlowMode resourceFlowMode;
 
-		public fuel(ConfigNode.Value cnv, bool bp = false) {
-			rid = PartResourceLibrary.Instance.GetDefinition(cnv.name).id;
-			rate = (bp ? -float.Parse(cnv.value) : float.Parse(cnv.value));
-			rfm = PartResourceLibrary.GetDefaultFlowMode(rid);
+		public Fuel(ConfigNode.Value nodeValue, bool bp = false) {
+			resourceID = PartResourceLibrary.Instance.GetDefinition(nodeValue.name).id;
+			rate = (bp ? -float.Parse(nodeValue.value) : float.Parse(nodeValue.value));
+			resourceFlowMode = PartResourceLibrary.GetDefaultFlowMode(resourceID);
 		}
 	}
 
 	public class emttr {
 		public string name;
-		public Color[] clrs;
+		public Color[] colors;
 		public float scale;
 
-		public emttr(ConfigNode.Value cnv) {
-			name = cnv.name;
+		public emttr(ConfigNode.Value nodeValue) {
+			name = nodeValue.name;
 			scale = 1;
 
-			string[] scs = cnv.value.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-			clrs = new Color[5];
+			string[] scs = nodeValue.value.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+			colors = new Color[5];
 
 			for(byte x = 0; x < 5; x++)
-				clrs[x] = ConfigNode.ParseColor(x > scs.Length - 1 ? "1,1,1" : scs[x] + "," + Mathf.Clamp(6f / x - 1.25f, 0f, 1f).ToString());	// Yes it's lame to convert to a string just to convert it back to a float, but I don't care
+				colors[x] = ConfigNode.ParseColor(x > scs.Length - 1 ? "1,1,1" : scs[x] + "," + Mathf.Clamp(6f / x - 1.25f, 0f, 1f).ToString());	// Yes it's lame to convert to a string just to convert it back to a float, but I don't care
 		}
 	}
 
 	public class light {
 		public string name;
-		public Color clr;
+		public Color color;
 		public Double mmag;
 
-		public light(ConfigNode.Value cnv, Part p) {
-			name = cnv.name;
-			clr = ConfigNode.ParseColor(cnv.value);
-			mmag = p.FindModelComponent<Light>(name).intensity;
+		public light(ConfigNode.Value value, Part part) {
+			name = value.name;
+			color = ConfigNode.ParseColor(value.value);
+			mmag = part.FindModelComponent<Light>(name).intensity;
 		}
 	}
 
 	public struct mode {
-		public fuel[] fuels, bypr;
+		public Fuel[] fuels, byproducts;
 		public ConfigNode.ValueList tanks;
 		public emttr[] emttrs;
 		public light[] lights;
 		public Double maxec;
 
-		public mode(ConfigNode cn, Part p) {
+		public mode(ConfigNode node, Part part) {
 			maxec = 10;
 
-			foreach(ConfigNode.Value cnv in cn.values ?? new ConfigNode.ValueList()) {
-				switch(cnv.name) {
+			foreach(ConfigNode.Value nodeValue in node.values ?? new ConfigNode.ValueList()) {
+				switch(nodeValue.name) {
 					case "MaxEC": {
-						maxec = float.Parse(cnv.value);
+						maxec = float.Parse(nodeValue.value);
 						break;
 					}
 				}
 			}
 
-			ConfigNode.ValueList vl = cn.GetNode("FUELS").values;	// No null coalescing intentional here
-			fuels = new fuel[vl.Count];
+			ConfigNode.ValueList nodeValues = node.GetNode("FUELS").values;	// No null coalescing intentional here
+			fuels = new Fuel[nodeValues.Count];
 
-			for(byte n = 0; n < vl.Count; n++)
-				fuels[n] = new fuel(vl[n]);
+			for(byte n = 0; n < nodeValues.Count; n++)
+				fuels[n] = new Fuel(nodeValues[n]);
 
-			vl = (cn.GetNode("BYPRODUCTS") ?? new ConfigNode()).values;
-			bypr = new fuel[vl.Count];
+			nodeValues = (node.GetNode("BYPRODUCTS") ?? new ConfigNode()).values;
+			byproducts = new Fuel[nodeValues.Count];
 
-			for(byte n = 0; n < vl.Count; n++)
-				bypr[n] = new fuel(vl[n], true);
+			for(byte n = 0; n < nodeValues.Count; n++)
+				byproducts[n] = new Fuel(nodeValues[n], true);
 
-			tanks = (cn.GetNode("TANKS") ?? new ConfigNode()).values;
+			tanks = (node.GetNode("TANKS") ?? new ConfigNode()).values;
 
-			vl = (cn.GetNode("EMITTERS") ?? new ConfigNode()).values;
-			emttrs = new emttr[vl.Count];
+			nodeValues = (node.GetNode("EMITTERS") ?? new ConfigNode()).values;
+			emttrs = new emttr[nodeValues.Count];
 
-			for(byte n = 0; n < vl.Count; n++)
-				emttrs[n] = new emttr(vl[n]);
+			for(byte n = 0; n < nodeValues.Count; n++)
+				emttrs[n] = new emttr(nodeValues[n]);
 
-			foreach(ConfigNode.Value cnv in (cn.GetNode("EMITSCALE") ?? new ConfigNode()).values)
+			foreach(ConfigNode.Value cnv in (node.GetNode("EMITSCALE") ?? new ConfigNode()).values)
 				Array.Find(emttrs, x => x.name == cnv.name).scale = float.Parse(cnv.value);
 
-			vl = (cn.GetNode("LIGHTS") ?? new ConfigNode()).values;
-			lights = new light[vl.Count];
+			nodeValues = (node.GetNode("LIGHTS") ?? new ConfigNode()).values;
+			lights = new light[nodeValues.Count];
 
-			for(byte n = 0; n < vl.Count; n++)
-				lights[n] = new light(vl[n], p);
+			for(byte i = 0; i < nodeValues.Count; i++)
+				lights[i] = new light(nodeValues[i], part);
 		}
 	}
 
 	public struct cfg {
-		public mode[] ms;
-		public float sh;
-		public int em;
+		public mode[] modes;
+		public float scaleHack;
+		public int emissionCoefficient;
 
-		public cfg(ConfigNode cn, Part p) {
-			sh = 1;
-			em = 50;
+		public cfg(ConfigNode node, Part part) {
+			scaleHack = 1;
+			emissionCoefficient = 50;
 
-			foreach(ConfigNode.Value v in cn.values ?? new ConfigNode.ValueList()) {
+			foreach(ConfigNode.Value v in node.values ?? new ConfigNode.ValueList()) {
 				switch(v.name) {
 					case "ScaleHack": {
-						sh = float.Parse(v.value);
+						scaleHack = float.Parse(v.value);
 						break;
 					}
 					case "EmitCoeff": {
-						em = int.Parse(v.value);
+						emissionCoefficient = int.Parse(v.value);
 						break;
 					}
 				}
 			}
 
-			ConfigNode[] nds = cn.GetNodes("MODE");	// Shouldn't need null coalescing as we should always have at least one MODE
-			ms = new mode[nds.Length];
+			ConfigNode[] nodes = node.GetNodes("MODE");	// Shouldn't need null coalescing as we should always have at least one MODE
+			modes = new mode[nodes.Length];
 
-			for(byte n = 0; n < nds.Length; n++)
-				ms[n] = new mode(nds[n], p);
+			for(byte i = 0; i < nodes.Length; i++)
+				modes[i] = new mode(nodes[i], part);
 		}
 	}
 }
