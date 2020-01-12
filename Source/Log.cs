a66f4efa3736d341aa659b871b22be8e773711cd
Lisias <me@lisias.net>
2020-01-12 00:15:31 -0300

    No need to guard the code under "#if DEBUG", as the ConditionalAtribute automatically omits the code when DEBUG is not active - and without breaking the callers! :)

--- Source/Log.cs
+++ Source/Log.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define DEBUG
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,29 +11,11 @@ namespace ODFC
 {
     class Log
     {
-        /// <summary>
-        /// sends the specific message to ingame mail and screen if Debug is defined
-        /// For debugging use only.
-        /// </summary>
-        /// <param name="msg">The MSG.</param>
-        /// <param name="params">The parameters.</param>
         [ConditionalAttribute("DEBUG")]
-        internal static void dbg(string msg, params object[] @params)
+        internal static void dbg(string msg)
         {
-            if (0 != @params.Length) msg = string.Format(msg, @params);
             ScreenMessages.PostScreenMessage(msg, 1, ScreenMessageStyle.UPPER_CENTER, true);
-            // for ingame mail 
             UnityEngine.Debug.Log(msg);
-            DebugLog(msg);
         }
-
-        /// <summary>
-        ///   sends the specific message to the game log if Debug is defined
-        ///   For debugging use only.
-        /// </summary>
-        /// <param name="m">The m.</param>
-        [Conditional(conditionString: "DEBUG")]
-        private static void DebugLog(object m) => UnityEngine.Debug.LogError(message: $"[ODFC]: {m}");
-
     }
 }
