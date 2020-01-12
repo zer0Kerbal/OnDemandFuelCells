1340aed3257f6b77990eeda9670c28a236194971
Lisias <me@lisias.net>
2020-01-12 00:45:48 -0300

    Better logging

--- Source/Log.cs
+++ Source/Log.cs
@@ -12,8 +12,9 @@ namespace ODFC
     class Log
     {
         [ConditionalAttribute("DEBUG")]
-        internal static void dbg(string msg)
+        internal static void dbg(string msg, params object[] @params)
         {
+            if (0 != @params.Length) msg = string.Format(msg, @params);
             ScreenMessages.PostScreenMessage(msg, 1, ScreenMessageStyle.UPPER_CENTER, true);
             UnityEngine.Debug.Log(msg);
         }
