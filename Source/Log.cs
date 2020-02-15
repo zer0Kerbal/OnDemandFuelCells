f3fcfb4863d7710bec5b8f0d74cdc86f04b11766
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-02-15 03:36:48 -0600

    Update Log.cs

--- Source/Log.cs
+++ Source/Log.cs
@@ -21,7 +21,7 @@ namespace ODFC
             if (0 != @params.Length) msg = string.Format(msg, @params);
             ScreenMessages.PostScreenMessage(msg, 1, ScreenMessageStyle.UPPER_CENTER, true);
             //Logg.Info(msg);
-            UnityEngine.Debug.Log("[ODFC TweakScale] " + msg);
+            //? UnityEngine.Debug.Log("[ODFC TweakScale] " + msg);
         }
     }
     internal static class Logg
