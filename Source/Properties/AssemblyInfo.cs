917524a7cefc55dbfb3e694321f8d49534a99fd3
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-07-30 21:49:20 -0500

    Update AssemblyInfo.cs

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,18 +1,39 @@
-﻿using System.Reflection;
+﻿#region license
+/*  On Demand Fuel Cells (ODFC) is a plugin to simulate fuel cells in 
+    Kerbal Space Program (KSP), and do a better job of it than stock's
+    use of a resource converter.
+    
+    Copyright (C) 2014 by Orum
+    Copyright (C) 2017, 2022 by Orum and zer0Kerbal (zer0Kerbal at hotmail dot com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
-
-// using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("On Demand Fuel Cells")]
+[assembly: AssemblyTitle("On Demand Fuel Cells (ODFC)")]
 [assembly: AssemblyDescription("On Demand Fuel Cells expands the capabilities of Kerbal Space Program (KSP) fuel cells")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("zer0Kerbal and Okram Industries")]
 [assembly: AssemblyProduct("ODFC")]
-[assembly: AssemblyCopyright("GPLv3")]
+[assembly: AssemblyCopyright("GPL-3.0 2014, 2017, 2022 by Orum and zer0Kerbal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,16 +45,4 @@ using System.Runtime.CompilerServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2fefd366-35f6-4219-a448-7de4054204f7")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersionAttribute(OnDemandFuelCells.Version.Text)]
-// [assembly: AssemblyFileVersion(OnDemandFuelCells.Version.Text)]
-// [assembly: AssemblyFileVersion("1.2.0.2")]
+//[assembly: AssemblyInformationalVersionAttribute(OnDemandFuelCells.Version.Text)]
