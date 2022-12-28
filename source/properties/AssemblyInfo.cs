2bddc5bfff3389c63c1a9d4aec3fd7974c6dcf33
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-12-28 00:00:20 -0600

    Create AssemblyInfo.cs

---
+++ source/properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿#region license
+/*  On Demand Fuel Cells (ODFC) is a plugin to simulate fuel cells in 
+    Kerbal Space Program (KSP), and do a better job of it than stock's
+    use of a resource converter.
+    
+    Copyright (C) 2014 by Orum
+    Copyright (C) 2017, 2022 by Orum and zer0Kerbal (zer0Kerbal at hotmail dot com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("On Demand Fuel Cells (ODFC)")]
+[assembly: AssemblyDescription("On Demand Fuel Cells expands the capabilities of Kerbal Space Program (KSP) fuel cells")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("zer0Kerbal and Okram Industries")]
+[assembly: AssemblyProduct("ODFC")]
+[assembly: AssemblyCopyright("GPL-3.0 2014, 2017, 2022 by Orum and zer0Kerbal")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2fefd366-35f6-4219-a448-7de4054204f7")]
